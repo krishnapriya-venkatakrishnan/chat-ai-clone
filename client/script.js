77eb8e6aac5f2476bf297f4ff6fbf332bbfc3ee3
Krishnapriya Venkatakrishnan <144596138+krishnapriya-venkatakrishnan@users.noreply.github.com>
2024-09-27 12:05:09 +0200

    Deployed backend server on render
    Deployed backend server on render

--- client/script.js
+++ client/script.js
@@ -21,7 +21,7 @@ const handleSubmit = async (e) => {
     const loaderIntervalId = loader(currBotMessage, uniqueId)
 
     // access the server
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://chat-ai-clone.onrender.com/", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
